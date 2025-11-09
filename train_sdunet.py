4800936ac0cd31279ec17c22d732857c2c28e41a
Gaargi H Amarendra <gaargiha@gmail.com>
2025-11-09 16:04:00 +0530

    Update train_sdunet.py

--- train_sdunet.py
+++ train_sdunet.py
@@ -9,29 +9,36 @@ from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as T
 import torch.nn.functional as F
 
+def dice(pred, target, eps=1e-3):
+    pred = pred.float()
+    target = target.float()
+    return (2 * (pred * target).sum() + eps) / (pred.sum() + target.sum() + eps)
+
+def iou(pred, target, eps=1e-3):
+    pred = pred.float()
+    target = target.float()
+    inter = (pred * target).sum()
+    union = pred.sum() + target.sum() - inter
+    return (inter + eps) / (union + eps)
+
 class MyISBIData(Dataset):
     def __init__(self, volPath, lblPath, resize=(128,128)):
         self.vols = tifffile.imread(volPath)
         self.lbls = tifffile.imread(lblPath)
-
         self.imgTransform = T.Compose([
             T.ToPILImage(),
             T.Resize(resize),
             T.ToTensor(),
             T.Normalize([0.5],[0.5])
         ])
-
         self.maskTransform = T.Compose([
             T.ToPILImage(),
             T.Resize(resize),
             T.ToTensor(),
             T.Lambda(lambda x: (x>0.5).float())
         ])
-
-
     def __len__(self):
         return len(self.vols)
-
     def __getitem__(self, index):
         img = self.vols[index]
         mask = self.lbls[index]
@@ -51,9 +58,10 @@ class DepthConv(nn.Module):
         super().__init__()
         self.depthConv = WeightConv(inChannel, inChannel, 3, padding=1, groups=inChannel)
         self.pointConv = nn.Conv2d(inChannel, outChannel, 1)
-
     def forward(self, inputTensor):
-        return self.pointConv(self.depthConv(inputTensor))
+        temp = self.depthConv(inputTensor)
+        out = self.pointConv(temp)
+        return out
 
 class SDUNet(nn.Module):
     def __init__(self, inChannel=1, outChannel=1):
@@ -70,34 +78,32 @@ class SDUNet(nn.Module):
         self.up2 = nn.ConvTranspose2d(128,64,2,2)
         self.up1 = nn.ConvTranspose2d(64,32,2,2)
         self.outputLayer = nn.Conv2d(32, outChannel, 1)
-
     def forward(self, inputTensor):
-        e1 = self.encoder1(inputTensor)
-        e2 = self.encoder2(self.pool(e1))
-        e3 = self.encoder3(self.pool(e2))
-        b = self.middle(self.pool(e3))
-        d3 = self.decoder3(torch.cat([self.up3(b), e3], 1))
-        d2 = self.decoder2(torch.cat([self.up2(d3), e2], 1))
-        d1 = self.decoder1(torch.cat([self.up1(d2), e1], 1))
-        return self.outputLayer(d1)
+        enc1 = self.encoder1(inputTensor)
+        enc2 = self.encoder2(self.pool(enc1))
+        enc3 = self.encoder3(self.pool(enc2))
+        bottleneck = self.middle(self.pool(enc3))
+        dec3 = self.decoder3(torch.cat([self.up3(bottleneck), enc3], 1))
+        dec2 = self.decoder2(torch.cat([self.up2(dec3), enc2], 1))
+        dec1 = self.decoder1(torch.cat([self.up1(dec2), enc1], 1))
+        out = self.outputLayer(dec1)
+        return out
 
 class ComboLoss(nn.Module):
     def __init__(self, alpha=0.5):
         super().__init__()
         self.alpha = alpha
         self.bceLoss = nn.BCEWithLogitsLoss()
-
     def diceLoss(self, pred, target, eps=1e-6):
         p = torch.sigmoid(pred)
         return 1 - (2*(p*target).sum() + eps) / (p.sum() + target.sum() + eps)
-
     def forward(self, pred, target):
         return self.alpha * self.bceLoss(pred, target) + (1 - self.alpha) * self.diceLoss(pred, target)
-
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 volPath = "/content/ISBI2012/train-volume.tif"
 labelPath = "/content/ISBI2012/train-labels.tif"
+
 data = MyISBIData(volPath, labelPath, resize=(128,128))
 trainSize = int(0.8 * len(data))
 trainSet, valSet = torch.utils.data.random_split(data, [trainSize, len(data)-trainSize])
@@ -115,22 +121,32 @@ for epoch in range(50):
     for images, masks in trainLoader:
         images, masks = images.to(device), masks.to(device)
         optimizer.zero_grad()
-        loss = lossFunction(model(images), masks)
+        preds = model(images)
+        loss = lossFunction(preds, masks)
         loss.backward()
         optimizer.step()
         trainLoss += loss.item()
-
+    
     model.eval()
     valLoss = 0
+    valDice = 0
+    valIoU = 0
     with torch.no_grad():
         for images, masks in valLoader:
             images, masks = images.to(device), masks.to(device)
-            valLoss += lossFunction(model(images), masks).item()
-
+            outputs = torch.sigmoid(model(images))
+            valLoss += lossFunction(outputs, masks).item()
+            preds = (outputs > 0.5).float()
+            valDice += dice(preds, masks)
+            valIoU += iou(preds, masks)
+    
     avgTrain = trainLoss / len(trainLoader)
     avgVal = valLoss / len(valLoader)
-    print("Epoch:", epoch+1, "| Train Loss:", round(avgTrain,4), "| Val Loss:", round(avgVal,4))
-
+    avgDice = valDice / len(valLoader)
+    avgIoU = valIoU / len(valLoader)
+    
+    print(f"Epoch {epoch+1} | Train Loss: {avgTrain:.4f} | Val Loss: {avgVal:.4f} | Dice: {avgDice:.4f} | IoU: {avgIoU:.4f}")
+    
     if avgVal < bestVal:
         bestVal = avgVal
         torch.save(model.state_dict(), "bestSDUNet.pth")
@@ -142,15 +158,18 @@ samples = random.sample(range(len(valSet)), 4)
 with torch.no_grad():
     for i, index in enumerate(samples):
         image, mask = valSet[index]
-        predict = torch.sigmoid(model(image.unsqueeze(0).to(device))).squeeze().cpu() > 0.5
-        plt.subplot(4,3,i*3+1)
+        output = torch.sigmoid(model(image.unsqueeze(0).to(device))).squeeze().cpu()
+        predict = (output > 0.5).float()
+        dice_score = dice(predict, mask)
+        iou_score = iou(predict, mask)
+        plt.subplot(4, 3, i*3 + 1)
         plt.imshow(image.squeeze(), cmap='gray')
-        plt.title('Input image data')
-        plt.subplot(4,3,3 *i+2)
+        plt.title('Input Image')
+        plt.subplot(4, 3, i*3 + 2)
         plt.imshow(mask.squeeze(), cmap='gray')
         plt.title('Ground Truth')
-        plt.subplot(4,3,i*3+3)
+        plt.subplot(4, 3, i*3 + 3)
         plt.imshow(predict, cmap='gray')
-        plt.title('Our Prediction')
+
 plt.tight_layout()
 plt.show()
